aca4069814d6909de6ffa4bca8bc12cfb22254a0
John Crepezzi <john.crepezzi@patch.com>
2010-08-27 13:15:59 -0500

    Added a user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+
+  before_filter :load_user, :only => [:show]
+  
+  def show
+  end
+
+  private
+
+  def load_user
+    begin
+      @user = User.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render_404
+      return false
+    end
+  end
+  
 end
