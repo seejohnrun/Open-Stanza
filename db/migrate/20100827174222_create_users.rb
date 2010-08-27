0ee2d16242304bce5171736b8da7859e8095e6bd
John Crepezzi <john.crepezzi@patch.com>
2010-08-27 12:44:03 -0500

    Added a user model

---
+++ db/migrate/20100827174222_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :public_name, :null => false
+      t.string :email, :null => false
+      t.boolean :suspended, :default => false, :null => false
+      
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100827160619) do
+ActiveRecord::Schema.define(:version => 20100827174222) do
 
   create_table "slugs", :force => true do |t|
     t.string   "name"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(:version => 20100827160619) do
     t.string   "cached_slug"
     t.boolean  "public",       :default => false, :null => false
     t.datetime "published_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "public_name",                    :null => false
+    t.string   "email",                          :null => false
+    t.boolean  "suspended",   :default => false, :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
