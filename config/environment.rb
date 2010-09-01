87a048bda035406b526b1152ccf546cb27d26d78
John Crepezzi <john.crepezzi@patch.com>
2010-09-01 16:08:37 -0400

    Freeze gem versions

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,3 @@
-# Be sure to restart your server when you modify this file
-
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
@@ -7,28 +5,14 @@ RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded.
-
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
-  config.gem 'haml'
-  config.gem 'friendly_id'
-  config.gem 'compass', :version => '>= 0.10.4'
-  config.gem 'authlogic'
+  config.gem 'haml', :version => '3.0.17'
+  config.gem 'friendly_id', :version => '3.0.6'
+  config.gem 'compass', :version => '0.10.4'
+  config.gem 'authlogic', :version => '2.1.5'
   
-  # Specify gems that this application depends on and have them installed with rake gems:install
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
-
-  # Only load the plugins named here, in the order given (default is alphabetical).
-  # :all can be used as a placeholder for all plugins not explicitly named
-  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
   # Skip frameworks you're not going to use. To use Rails without a database,
   # you must remove the Active Record framework.
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
