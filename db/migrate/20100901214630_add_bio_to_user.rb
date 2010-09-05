ce67c27bbf43dfd62d5a0710e4afc0b02c31d0e1
John Crepezzi <john.crepezzi@patch.com>
2010-09-05 17:11:21 -0400

    Fix migration

--- db/migrate/20100901214630_add_bio_to_user.rb
+++ db/migrate/20100901214630_add_bio_to_user.rb
@@ -1,6 +1,6 @@
 class AddBioToUser < ActiveRecord::Migration
   def self.up
-    add_column :users, :biography, :text, :limit => 5000, :null => true
+    add_column :users, :biography, :text, :null => true
   end
 
   def self.down
